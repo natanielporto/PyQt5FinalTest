8c8e13205d5bf7dabf1ea3effda348ee2b725e36
Nataniel Porto <natanielkporto@gmail.com>
2020-06-22 01:18:28 -0300

    first commit

---
+++ banco.py
@@ -0,0 +1,20 @@
+from mysql.connector import connect
+from contextlib import contextmanager
+
+config_db = dict(
+  host = "localhost",
+  port = "6006",
+  user = "root",
+  database = "music"
+)
+
+@contextmanager
+def db_connect():
+  connection = connect(**config_db)
+  try:
+    yield connection
+  finally:
+    if connection and connection.is_connected():
+      connection.close()
+
+      
